57045e4186f5128526f6aa7bc1cbfcd4b69be633
TheNani9999 <vamshikrishnagadde9999@gmail.com>
2025-01-29 13:41:28 -0700

    Update boustrophedon_controller.py

--- assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
+++ assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
@@ -19,11 +19,11 @@ class BoustrophedonController(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('Kp_linear', 10.0),
-                ('Kd_linear', 0.1),
-                ('Kp_angular', 5.0),
-                ('Kd_angular', 0.2),
-                ('spacing', 1.0)
+                ('Kp_linear', 8.0),
+                ('Kd_linear', 0.05),
+                ('Kp_angular', 7.0),
+                ('Kd_angular', 0.000),
+                ('spacing', 0.5)
             ]
         )
         
